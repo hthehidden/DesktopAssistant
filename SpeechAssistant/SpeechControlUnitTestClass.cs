db749cd87c1316c3a66733201ceea7d746b7d0bc
hgraham3 <hgraham3@asu.edu>
2018-07-08 20:00:37 -0700

    adding unit tests,

---
+++ SpeechAssistant/SpeechControlUnitTestClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechAssistant
+{
+    public class SpeechControlUnitTestClass : SpeechControl
+    {
+        public static int stateChangeValue { get; set; }
+        public override void setUp(SpeechWrapper speechWrapper)
+        {
+            stateChangeValue = 0;
+            addPhraseCommand(new PhraseCommandTest().create(speechWrapper.initWord));
+        }
+
+        class PhraseCommandTest : PhraseCommand
+        {
+            public override void commandProcess(string commandInput)
+            {
+                stateChangeValue = 1;
+            }
+
+            public override void Grammar_SpeechRecognized(SpeechRecognizedEventArgs e)
+            {
+                if ((bool)e.Result.Semantics["true_false"].Value)
+                    stateChangeValue = 2;
+            }
+
+            protected override void defineCommands(string initWord)
+            {
+                addCommand("run unit test");
+                Choices cs = new Choices();
+                cs.Add(new SemanticResultValue("true", true));
+                GrammarBuilder gb = new GrammarBuilder();
+                gb.Append(new SemanticResultKey("true_false", cs));
+                this.grammar = new Grammar(gb);
+            }
+        }
+    }
+}
---
+++ SpeechAssistantTests/DataManagerTests.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpeechAssistant;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SpeechAssistant.Tests
+{
+    [TestClass()]
+    public class DataManagerTests
+    {
+        [TestMethod()]
+        public void addLocationTest()
+        {
+            DataManager manager = new DataManager();
+            manager.addLocation(Environment.SpecialFolder.AdminTools, "admin tools");
+            Assert.IsTrue(manager.directoryLocations[Environment.SpecialFolder.AdminTools].Contains("admin tools"));
+
+        }
+
+        [TestMethod()]
+        public void removeLocationTest()
+        {
+            DataManager manager = new DataManager();
+            manager.addLocation(Environment.SpecialFolder.AdminTools, "admin tools");
+            Assert.IsTrue(manager.directoryLocations[Environment.SpecialFolder.AdminTools].Contains("admin tools"));
+            manager.removeLocation(Environment.SpecialFolder.AdminTools, "admin tools");
+            Assert.IsTrue(!manager.directoryLocations[Environment.SpecialFolder.AdminTools].Contains("admine tools"));
+        }
+
+        [TestMethod()]
+        public void getManagerTest()
+        {
+
+            DirectoryInfo dirInfo = Directory.CreateDirectory("DataManagerUnitTestDirectory");
+            string file = string.Format("{0}\\{1}", dirInfo.Name, "DataManagerUnitTestFile.dm");
+            try
+            {
+                DataManager manager = new DataManager();
+                DataManager.Save(manager, file);
+                manager = null;
+                manager = DataManager.getManager(dirInfo.Name);
+                Assert.IsTrue(manager != null);
+            }
+            catch(Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+            finally
+            {
+                File.Delete(file);
+                dirInfo.Delete();
+            }
+        }
+
+
+        [TestMethod()]
+        public void DatamanagerConstructorTest()
+        {
+            try
+            {
+                new DataManager();
+            }
+            catch(Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            DataManager manager = new DataManager();
+            string file = "DataManagerUnitTestFile.dam";
+            bool written = false;
+            try
+            {
+                DataManager.Save(manager, file);
+                written = true;
+            }
+            catch(Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+            finally
+            {
+                if (written)
+                    File.Delete(file);
+            }
+        }
+        [TestMethod()]
+        public void IsSerializable()
+        {
+            DataManager manager = new DataManager();
+            manager.addLocation(Environment.SpecialFolder.AdminTools, "admin tools");
+            using (MemoryStream mem = new MemoryStream())
+            {
+                BinaryFormatter b = new BinaryFormatter();
+                try
+                {
+                    b.Serialize(mem,manager);
+                }
+                catch(Exception ex)
+                {
+                    Assert.Fail(ex.Message);
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void LoadTest()
+        {
+            DataManager manager = new DataManager();
+            manager.addLocation(Environment.SpecialFolder.AdminTools, "admin tools");
+            Assert.IsTrue(manager.directoryLocations[Environment.SpecialFolder.AdminTools].Contains("admin tools"));
+            string file = "DataManagerUnitTestFile.dam";
+            bool written = false;
+            try
+            {
+                DataManager.Save(manager, file);
+                written = true;
+                manager = new DataManager();
+                Assert.IsFalse(manager.directoryLocations.Keys.Contains(Environment.SpecialFolder.AdminTools));
+                manager = DataManager.Load(file);
+                Assert.IsTrue(manager.directoryLocations[Environment.SpecialFolder.AdminTools].Contains("admin tools"));
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+            finally
+            {
+                if (written)
+                    File.Delete(file);
+            }
+        }
+
+    }
+}
